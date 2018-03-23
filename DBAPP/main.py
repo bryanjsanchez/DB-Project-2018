415450909916c0400f2470823ca18040fb0a4384
saleur <luis.ruelas@upr.edu>
2018-03-23 16:30:07 -0400

    Added UserDAO and UserHandler

--- DBAPP/main.py
+++ DBAPP/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
 
+from handler.users import UserHandler
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -11,6 +12,13 @@ def hello():
 @app.route('/login/')
 def log():
     return "No logging!"
+@app.route('/ChatApp/users/<int:uid>')
+def getUserById(uid):
+    return UserHandler().getUserById(uid)
+
+@app.route('/ChatApp/users/<int:uid>/contacts')
+def getContactsById(uid):  
+    return UserHandler().getContactsById(uid)
 
 if __name__ == '__main__':
     app.run(debug=True)
