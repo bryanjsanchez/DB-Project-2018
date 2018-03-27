1a757a40b8ff75a1cc3a0242bf303fb018171b1d
bryanjsanchez <bryanjsanchez@gmail.com>
2018-03-27 12:31:07 -0400

    Add Hashtag routes and modify Handler and Dao

--- DBAPP/dao/hashtag.py
+++ DBAPP/dao/hashtag.py
@@ -1,35 +1,38 @@
 class HashtagDAO:
 
     def __init__(self):
-        H1 = [101, 'Hello', 120]
-        H2 = [102, 'NewYear', 120]
-        H3 = [103, 'DoingGood', 121]
-        H4 = [104, 'NewYear', 121]
-        H5 = [105, 'LetsParty', 122]
-        H6 = [106, 'YOLO', 122]
-        H7 = [107, 'YOLO', 123]
+        H1 = [101, 'Hello', 1]
+        H2 = [102, 'NewYear', 2]
+        H3 = [103, 'DoingGood', 1]
+        H4 = [104, 'LetsParty', 1]
+        H5 = [105, 'YOLO', 2]
         self.data = []
         self.data.append(H1)
         self.data.append(H2)
         self.data.append(H3)
         self.data.append(H4)
         self.data.append(H5)
-        self.data.append(H6)
-        self.data.append(H7)
     
     def getAllHashtags(self):
         return self.data
+
+    def getHashtagByID(self, hid):
+        for h in self.data:
+            if h[0] == hid:
+                return h
+        return None
 
     def getAllHashtagsByText(self, text):
         hashtags = []
         for h in self.data:
             if h[1] == text:
                 hashtags.append(h)
+        if not hashtags:
+            return None
         return hashtags
 
-    def getAllHashtagsByMessage(self, messageid):
-        hashtags = []
-        for h in self.data:
-            if h[2] == messageid:
-                hashtags.append(h)
-        return hashtags
+    def getTop10Hashtags(self):
+        self.data.sort(key=lambda x:x[2], reverse=True)
+        return self.data[:10]
+
+
