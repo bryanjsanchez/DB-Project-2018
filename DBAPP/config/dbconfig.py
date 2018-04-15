0a4cf48e89557b5a88d47cbd9f91a35b6ed3be4d
bryanjsanchez <bryanjsanchez@gmail.com>
2018-04-15 18:48:06 -0400

    Add dbconfig and partial hashtag dao implementation

---
+++ DBAPP/config/dbconfig.py
@@ -0,0 +1,8 @@
+#Database configuration information
+
+pg_config = {
+    'user': 'appusr"',
+    'password': 'password',
+    'dbname': 'appdb',
+    'host': 'localhost'
+}
--- DBAPP/dao/hashtag.py
+++ DBAPP/dao/hashtag.py
@@ -1,20 +1,38 @@
+from config.dbconfig import pg_config
+import psycopg2
 class HashtagDAO:
 
     def __init__(self):
-        H1 = [101, 'Hello', 1]
-        H2 = [102, 'NewYear', 2]
-        H3 = [103, 'DoingGood', 1]
-        H4 = [104, 'LetsParty', 1]
-        H5 = [105, 'YOLO', 2]
-        self.data = []
-        self.data.append(H1)
-        self.data.append(H2)
-        self.data.append(H3)
-        self.data.append(H4)
-        self.data.append(H5)
+
+        connection_url = "dbname=%s user=%s host=%s password=%s" % (pg_config['dbname'],
+                                                            pg_config['user'],
+                                                            pg_config['host'],
+                                                            pg_config['password'])
+        self.conn = psycopg2._connect(connection_url);
+
+        #########Data from phase 1#######
+        #                               #
+        # H1 = [101, 'Hello', 1]        #
+        # H2 = [102, 'NewYear', 2]      #
+        # H3 = [103, 'DoingGood', 1]    #
+        # H4 = [104, 'LetsParty', 1]    #
+        # H5 = [105, 'YOLO', 2]         #
+        # self.data = []                #
+        # self.data.append(H1)          #
+        # self.data.append(H2)          #
+        # self.data.append(H3)          #
+        # self.data.append(H4)          #
+        # self.data.append(H5)          #
+        #################################
     
     def getAllHashtags(self):
-        return self.data
+        cursor = self.conn.cursor()
+        query = "select * from hashtag;"
+        cursor.execute(query);
+        result = []
+        for row in cursor:
+            result.append(row);
+        return result
 
     def getHashtagByID(self, hid):
         for h in self.data:
