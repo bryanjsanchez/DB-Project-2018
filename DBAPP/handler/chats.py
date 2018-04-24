a4e2fb03be1abe40bb89a522f89712b5a79c5866
RourkeGoat <eduardo.rivera8@upr.edu>
2018-04-24 01:07:34 -0400

    Add files via upload

--- DBAPP/handler/chats.py
+++ DBAPP/handler/chats.py
@@ -27,6 +27,21 @@ class ChatHandler:
         result['dislikes'] = row[7]
         return result
 
+    def mapOwnersToDict(self, row):
+        result = {}
+        result['uid'] = row[0]
+        result['uusername'] = row[1]
+        result['cgname'] = row[2]
+        return result
+
+    def mapMembersToDict(self, row):
+        result = {}
+        result['uid'] = row[0]
+        result['uusername'] = row[1]
+        result['cgname'] = row[2]
+        return result
+
+
     def getAllChatGroups(self):
         dao = ChatDAO()
         result = dao.getAllChatGroups()
@@ -75,4 +90,26 @@ class ChatHandler:
             return jsonify(Error="Not Found"), 404
         for r in result:
             mapped_result.append(self.mapToDict(r))
-        return jsonify(Hashtag=mapped_result)
+        return jsonify(Chat=mapped_result)
+
+    def getChatOwner(self, cgid ):
+        dao = ChatDAO()
+        result = dao.getChatOwner(cgid)
+        mapped_result = []
+        if not result:
+            return jsonify(Error="Not Found"), 404
+        for r in result:
+            mapped_result.append(self.mapOwnersToDict(r))
+        return jsonify(Chat=mapped_result)
+
+    def getChatMembers(self, cgid):
+        dao = ChatDAO()
+        result = dao.getChatUsers(cgid)
+        mapped_result = []
+        if not result:
+            return jsonify(Error="Not Found"), 404
+        for r in result:
+            mapped_result.append(self.mapMembersToDict(r))
+        return jsonify(Members=mapped_result)
+
+
