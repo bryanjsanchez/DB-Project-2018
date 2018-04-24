659c76c222c22eaaf9e7822732ecadf64af463db
Luis <thekiwi67@gmail.com>
2018-04-24 15:53:39 -0400

    Modified the postMsg function in chat.js to make it compatible with our configurarion of chat.html

--- ChatApp/appjs/chat.js
+++ ChatApp/appjs/chat.js
@@ -3,13 +3,14 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
         var thisCtrl = this;
 
         this.messageList = [];
-        this.counter  = 2;
+        this.counter;
         this.newText = "";
 
         this.loadMessages = function(){
             // Get the messages from the server through the rest api
             $http.get('http://127.0.0.1:5000/ChatApp/chat/1/messages').then(function(response) {
                 thisCtrl.messageList = response.data.Messages;
+                counter = thisCtrl.messageList.size()
             });
             $log.error("Message Loaded: ", JSON.stringify(thisCtrl.messageList));
         };
@@ -19,7 +20,7 @@ angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope
             // Need to figure out who I am
             var author = "Me";
             var nextId = thisCtrl.counter++;
-            thisCtrl.messageList.unshift({"id": nextId, "text" : msg, "author" : author, "like" : 0, "nolike" : 0});
+            thisCtrl.messageList.unshift({"id": nextId, "text" : msg, "username" : author, "likes" : 0, "dislikes" : 0});
             thisCtrl.newText = "";
         };
 
